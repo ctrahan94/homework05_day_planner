55abb568a6a52c0a5cc67ac39b3e10cc6a883964
Colby Trahan <c.trahan94@yahoo.com>
2020-10-04 19:16:36 -0600

    Gets the value from local storage and displays it on the webpage

--- script.js
+++ script.js
@@ -61,3 +61,16 @@ $(".saveBtn").on("click", function () {
   var key = $(this).parent().attr("id");
   localStorage.setItem(key, value);
 });
+
+//Get the value from local storage and displays it on the webpage
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));

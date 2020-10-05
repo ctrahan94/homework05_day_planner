a27d23021e2f7fd6c2b080ffe0a66f46ae410538
Colby Trahan <c.trahan94@yahoo.com>
2020-10-04 19:07:18 -0600

    Added a for loop that creates the div rows and columns

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ var displayedHours = [
 //Displays the current date and time
 var currentDay = dayjs().format("dddd, MMM D YYYY");
 $("#currentDay").append(currentDay);
+
+//Loop that creates the rows for each hour
+for (var i = 0; i < hour.length; i++) {
+  var createDiv = $("<div>");
+  createDiv
+    .attr("class", "row time-block")
+    .attr("id", "hour-" + hour[i])
+    .attr("value", hour[i]);
+  //Creates the text area
+  var createTextArea = $("<textarea>");
+  $(".container").append(createDiv);
+
+  //Puts the displayedHours on the left side
+  var currentTime = displayedHours[i];
+  createDiv.append($("<div class = 'col-md-1 hour'>").text(currentTime));
+
+  //The text area across 10 columns
+  createDiv.append(
+    createTextArea
+      .attr("class", "col-md-10 description")
+      .attr("id", "memo-area")
+  );
